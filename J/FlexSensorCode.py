a6218460c2f1ef8566d0b4ab60f805bc95d5d6ae
jastackhouse <35642383+jastackhouse@users.noreply.github.com>
2018-04-12 17:28:20 -0600

    Yo, just used the trimpot code for the other force and flex sensors, and guess what??? It worked!!

---
+++ J/FlexSensorCode.py
@@ -0,0 +1,26 @@
+import time
+import spidev
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 10000000
+
+def readAdc(channel):
+        #Read the raw data for channel 0 using the xfer2 method, which
+        #sends AND recieves depending on the clock rise/fall.
+        r = spi.xfer([int("01100000",2),15])
+
+        #Get data
+        #get 10 bit bitsring from r[0]
+        s = bin(r[0])[2:].zfill(10)
+        #append 8 '0's to last 2 bits from r[0]
+        data = int(s[8:] + "0"*8, 2) +r[1]
+        return data
+while True:
+    reading = readAdc(0)
+    if reading > 600:
+        print("Success")
+        break
+    else:
+        print("Waiting")
+    time.sleep(1)
