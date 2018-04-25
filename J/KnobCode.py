c98f58e2ea39a4a054fff0812131a9a7d48a7c8a
jastackhouse <35642383+jastackhouse@users.noreply.github.com>
2018-04-24 23:03:01 -0600

    master sensor code for all sensors on breadboard. works most of the time. still debugging and checking for correct outputs given all the sensor inputs

--- J/KnobCode.py
+++ J/KnobCode.py
@@ -1,4 +1,3 @@
-import numpy as np
 import time as t
 import spidev
 
--- J/MasterSensorCode.py
+++ J/MasterSensorCode.py
@@ -2,6 +2,8 @@
 #4/17/18
 #Code for all of the sensors and mechanical inputs to be used in Rock Boy and Mineral Girl"
 
+
+
 import time
 import smbus
 import RPi.GPIO as GPIO
@@ -10,9 +12,12 @@ import spidev
 Import smbus for I2C connection for accelerometer
 Import RPi.GPIO to control pins on the Pi Wedge
 Import spidev for reading inputs from analog sensors through ADC chip"""
+evalTime = 10
+
+GPIO.setwarnings(False)
 
 #Fucntion that checks for changes in acclerometer readings to simulate taking strike and dip
-def accelTest(strike,dip):
+def accelCode():
     #number of bits in the I2C input
     bits = 16
     #list of previous readings for comparison
@@ -20,17 +25,17 @@ def accelTest(strike,dip):
     #get I2C bus - initial bus to channel 1
     bus = smbus.SMBus(1)
     
-    while True:
+    for i in range(evalTime):
         #put register in active mode
         bus.write_byte_data(0x1D, 0x2A, 1)
-        time.sleep(0.25)
+        time.sleep(0.05)
 
         #read from status register
         data = bus.read_i2c_block_data(0x1D, 0x00, 7)
 
         #put register in standby mode
         bus.write_byte_data(0x1D, 0x2A, 0)
-        time.sleep(0.25)
+        time.sleep(0.05)
 
         #convert input data into individual values for x, y, and z
         acclList = []
@@ -43,20 +48,16 @@ def accelTest(strike,dip):
         #compare previous data to current data to detect if accelerometer has moved
         if len(prvList) > 0:
             if abs(acclList[0]-prvList[0]) > 100 or abs(acclList[2] - prvList[2]) > 100:
-                print("Stirke and Dip Measured!!")
-                print(strike, dip)
+                return True
                 break
-        else:
-            print("Waiting")
 
         #store current data for comparison in next cycle of loop
         prvList = acclList
-
-    #return output for next function
-    return
+        print("Waiting")
+    return False
 
 #Function that checks for button inputs
-def buttonTest():
+def buttonCode():
     #set the pins for the different colored buttons
     bluePin = 27
     greenPin = 26
@@ -70,34 +71,28 @@ def buttonTest():
     GPIO.setup(redPin, GPIO.IN, GPIO.PUD_DOWN)
 
     #check for button presses
-    while True:
+    for i in range(evalTime):
         blueInput = GPIO.input(bluePin)
         greenInput = GPIO.input(greenPin)
         yellowInput = GPIO.input(yellowPin)
         redInput = GPIO.input(redPin)
         if blueInput == 1:
-            print("Blue button pressed")
-            return
+            return "Blue"
             break
         elif greenInput == 1:
-            print("Green button pressed")
-            return
+            return "Green"
             break
         elif yellowInput == 1:
-            print("Yellow button pressed")
-            return
+            return "Yellow"
             break
         elif redInput == 1:
-            print("Red Button pressed")
-            return
+            return "Red"
             break
-        else:
-            print("Waiting for player input")
-        time.sleep(.5)
+        time.sleep(.1)
+    return False
 
 #Read ADC chip function
 def readAdc(channel):
-
     #create spidev object
     spi = spidev.SpiDev()
     #opens specific port and channel
@@ -114,39 +109,170 @@ def readAdc(channel):
     return data
 
 #Function that detects if the flex sensor is bent
-def flexTest():
-    #Initial preivous reading variable
-    prvReading = 0
-    #Count for tracking amount of times flex sensor reading changes
-    readingCount = 0
+def flexCode():
     #start tracking flex sensor readings
-    while True:
-        reading = readAdc(0)
-        print(reading)
-        if prvReading != 0:
-            #record amount of times readings change
-            if reading != prvReading:
-                readingCount += 1
-            else:
-                print("Waiting")
-        if readingCount == 3:
-            print("Tenacity Tested")
-            return
+    for i in range(evalTime):
+        firstReading = readAdc(0)
+        time.sleep(.1)
+        secondReading = readAdc(0)
+        #test if sensor is flexed
+        if firstReading - secondReading > 20:
+            return True
             break
-        time.sleep(.5)
-        #store reading to compare with next reading
-        prvReading = reading
+    return False    
 
 #Function that detecs pressure on force resist sensor
-def forceTest():
-    while True:
+def forceCode():
+    for i in range(evalTime):
+        #Takes in reading on Force Resistor
         reading = readAdc(0)
-        if reading > 600:
-            print("Fracture tested")
-            return
+        #Detects if applied forces exceed comparison value
+        if reading > 500:
+            return True
             break
-        else:
-            print("Waiting")
-        time.sleep(.5)
+        time.sleep(.1)
+    
+    return False
 
+#Function that detects when the Trimpot Knob is turned
+def knobCode():
+    for i in range(evalTime):
+        #First measurement of Trimpot position
+        first = readAdc(0)
+        #Delay between readings
+        time.sleep(.1)
+        #Second measurement of Trimpot position
+        second = readAdc(0)
+        #Comparison to check if knob has been rotated
+        if first - second > 10:
+            return True
+            break
+    
+    return False
+    
+def magnetCode():
+    #Sets pin to read Reed Switch input
+    reedPin = 18
+    #Set the correct board configuration
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(reedPin, GPIO.IN, GPIO.PUD_DOWN)
+
+    for i in range(evalTime):
+        #Takes in reading from Reed Swith
+        reading = GPIO.input(reedPin)
+        #Test if input is on
+        if reading == 1:
+            return True
+            break
+        time.sleep(.1)
+    return False
+
+#Bitstrings for photocell ADC chip readings
+def bitstring(n):
+    s = bin(n)[2:]
+    return "0"*(8-len(s)) + s
+
+#ADC chip reader code for photocell
+def read(adc_channel):
+    #Specify channel
+    spi_channel = 0
+    #Create Spidev object
+    spi2 = spidev.SpiDev()
+    #Open specific port and channel
+    spi2.open(adc_channel, spi_channel)
+    #set max speed
+    spi2.max_speed_hz = 1000000
+    #cmd corrections
+    cmd = 128
+    if adc_channel:
+        cmd += 32
+    #Read xfer2 method
+    reply_bytes = spi2.xfer2([cmd, 0])
+    #Join to bitstring
+    reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
+    #Slice reply
+    reply = reply_bitstring[5:15]
+    #return data formatted in base 10
+    return int(reply,2) / 2**10
+
+def photocellCode():
+    for i in range(evalTime):
+        firstRead = read(0)
+        time.sleep(.1)
+        secondRead = read(0)
+        print(firstRead, secondRead)
+        if firstRead == secondRead:
+            return True
+            break
+
+    return False
+
+def checkAll():
+    flexPin = 4
+    forcePin = 5
+    knobPin = 6
+    photoPin = 12
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(flexPin, GPIO.OUT)
+    GPIO.setup(forcePin, GPIO.OUT)
+    GPIO.setup(knobPin, GPIO.OUT)
+    GPIO.setup(photoPin, GPIO.OUT)
+
+    GPIO.output(flexPin, False)
+    GPIO.output(forcePin, False)
+    GPIO.output(knobPin, False)
+    GPIO.output(photoPin, False)
+    
+    pinList = [flexPin, forcePin, knobPin, photoPin]
+    while True:
+        for pin in pinList:
+            if pin == flexPin:
+                GPIO.output(flexPin, True)
+                if flexCode() == True:
+                    return "Flex"
+                    break
+                else:
+                    GPIO.output(flexPin, False)
+
+            if pin == forcePin:
+                GPIO.output(forcePin, True)
+                if forceCode() == True:
+                    return "Force"
+                    break
+                else:
+                    GPIO.output(forcePin, False)
+
+            if pin == knobPin:
+                GPIO.output(knobPin, True)
+                if knobCode() == True:
+                    return "Knob"
+                    break
+                else:
+                    GPIO.output(knobPin, False)
+
+            if pin == photoPin:
+                GPIO.output(photoPin, True)
+                if photocellCode() == True:
+                    return "Photocell"
+                    break
+                else:
+                    GPIO.output(photoPin, False)
+
+        if magnetCode() == True:
+            return "Magnet"
+            break
+
+        color = buttonCode()
+        if color != False:
+            return color
+            break
+
+        if accelCode() == True:
+            return "Accelerometer"
+            break
+print(accelCode())
+#print(checkAll())
+                
+    
 
